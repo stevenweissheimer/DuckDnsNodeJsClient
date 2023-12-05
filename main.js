60443617056801887ed6899ee14e7e0c80e76bb6
Steven Weißheimer <mail@stevenweissheimer.com>
2023-12-05 22:38:46 +0100

    add console input

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
+const fs = require('fs');
 const axios = require('axios');
+const readlineSync = require('readline-sync');
 const dotenv = require('dotenv');
 
 // Lade Umgebungsvariablen aus .env-Datei
 dotenv.config();
 
-const duckDnsDomain = process.env.DOMAIN;
-const duckDnsToken = process.env.TOKEN;
-const updateTime = (process.env.UPDATETIME * 60 * 1000);
+// Überprüfe, ob die .env-Datei vorhanden ist
+if (!fs.existsSync('.env')) {
+  // Frag die Werte von der Kommandozeile ab
+  const domain = readlineSync.question('Bitte gib deine DuckDNS-Domain (nur den ersten Teil) ein: ');
+  const token = readlineSync.question('Bitte gib deinen DuckDNS-Token ein: ');
+  const updateTime = readlineSync.question('Bitte gib das Update-Intervall in Minuten ein: ');
 
+  // Erstelle den Inhalt für die .env-Datei
+  const envContent = `DOMAIN=${domain}\nTOKEN=${token}\nUPDATETIME=${updateTime}\n`;
+
+  // Schreibe den Inhalt in die .env-Datei
+  fs.writeFileSync('.env', envContent);
+
+  console.log('.env-Datei erfolgreich erstellt.');
+}
+
+const duckDnsDomain = process.env.DOMAIN + ".duckdns.org";
+const duckDnsToken = process.env.TOKEN;
+const updateTime = process.env.UPDATETIME * 60 * 1000;
+
+// Restlicher Code für die DynDNS-Aktualisierung
 async function updateDynDns() {
   try {
     const response = await axios.get(`https://www.duckdns.org/update?domains=${duckDnsDomain}&token=${duckDnsToken}`);
